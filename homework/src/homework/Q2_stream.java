0f0c5b2d2b82b53d8bf865c77a24150b210e0ef9
gusdk <gusdk3105@naver.com>
2019-03-07 00:02:56 +0900

    why..

--- homework/src/homework/Q2_stream.java
+++ homework/src/homework/Q2_stream.java
@@ -1,9 +1,18 @@
 package homework;
 
+import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.SortedMap;
+import java.util.Vector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Q2_stream {
 
@@ -14,6 +23,7 @@ public class Q2_stream {
 		map.put(2, "ABCD");
 		map.put(3, "ABCDE");
 		
+		while(true) {
 		System.out.println("1.전체 조회 2.key로 조회 3.추가 4.수정 5.삭제 ");
 		Scanner scan = new Scanner(System.in);
 		int num = scan.nextInt();
@@ -37,27 +47,26 @@ public class Q2_stream {
 			
 			System.out.println("키를 입력하세요");
 			int k = scan.nextInt();
+			System.out.println("밸류를 입력하세요");
+			String val = scan.next();
 			if(map.containsKey(k)) {
 				System.out.println("이미 존재 합니다.");
 			}
 			else {
-				System.out.println("밸류를 입력하세요");
-				String v = scan.next();
-				
-				map.put(k,v);
-				map.entrySet().stream().forEach(entry -> System.out.println(entry.getKey()+"/"+entry.getValue()));
+			map.merge(k, val , (k1,v) ->{return k + v;} );
 			}
 			
 			break;
 		case 4:
+		
 			System.out.println("수정할 key를 입력하세요");
 			int kk = scan.nextInt();
+			String ss=scan.next();
+		
 			if(map.containsKey(kk)) {
-				System.out.println("밸류를 입력하세요");
-				String v = scan.next();
-				
-				map.put(kk,v);
-				map.entrySet().stream().forEach(entry -> System.out.println(entry.getKey()+"/"+entry.getValue()));
+				  map.entrySet().stream().filter(x -> x.getKey()
+						  == kk).forEach(u -> u.setValue(ss));
+						  
 			}else {
 				System.out.println("없음");
 			}
@@ -73,6 +82,7 @@ public class Q2_stream {
 		default:
 			System.out.println("이상한거 입력하면 종료");
 			break;
+		}
 		}
 	}
 
